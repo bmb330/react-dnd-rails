0e816a562ee9dc85aac2cf70aabb09301681bc56
Brandon Braun <brandon.braun@gmail.com>
2016-05-16 10:12:36 -0400

    generate gem

---
+++ lib/react/dnd/rails.rb
@@ -0,0 +1,9 @@
+require "react/dnd/rails/version"
+
+module React
+  module Dnd
+    module Rails
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/react/dnd/rails/version.rb
@@ -0,0 +1,7 @@
+module React
+  module Dnd
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
